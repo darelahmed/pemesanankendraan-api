b907447d8c67a9fa3645572783bdbe6dc0cc0d69
darrellahmed <darel.ahmedgalen@gmail.com>
2023-10-11 20:42:11 +0700

    add:[DAG]add table migrations

---
+++ database/migrations/2023_10_11_120245_vehicle_bookings.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicle_bookings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('vehicle_id');
+            $table->integer('approval_level');
+            $table->string('status'); // Status pemesanan (Menunggu persetujuan, Disetujui, Ditolak, dll)
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('vehicle_id')->references('id')->on('vehicles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicle_bookings');
+    }
+};
