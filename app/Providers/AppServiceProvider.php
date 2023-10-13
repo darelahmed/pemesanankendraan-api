e696efb98b08f87a8cb9d83c6514ed395e3ceef8
darrellahmed <darel.ahmedgalen@gmail.com>
2023-10-13 08:39:21 +0700

    add:[DAG]add client api store & get booking

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        Validator::extend('enum', function ($attribute, $value, $parameters, $validator) {
+            // Ganti ini dengan definisi validasi enum Anda.
+            $validEnums = ['Pending', 'Approved', 'Rejected', 'Bad', 'Normal', 'Good'];
+
+            return in_array($value, $validEnums);
+        });
     }
 }
